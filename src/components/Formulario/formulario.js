3603e5f17a2c45b9be5aeb52a5480051db641b54
Yber-M <ybermarlon@gmail.com>
2025-01-04 14:22:07 -0500

    Formulario

--- src/components/Formulario/formulario.js
+++ src/components/Formulario/formulario.js
@@ -4,10 +4,14 @@ import ListaOpciones from '../ListaOpciones';
 import Boton from '../Button';
 
 const Formulario = () => {
+    const manejarEnvio = (event) => {
+        event.preventDefault();
+        console.log('Hola', event);
+    }
     return <section className='formulario'>
-        <form>
+        <form onSubmit={manejarEnvio}>
             <h2>Rellena el formulario para crear el colaborador.</h2>
-            <CampoTexto titulo='Nombre' placeholder='Ingresar nombre'/>
+            <CampoTexto titulo='Nombre' placeholder='Ingresar nombre' required={true}/>
             <CampoTexto titulo='Puesto' placeholder='Ingresar su puesto'/>
             <CampoTexto titulo='Foto' placeholder='Ingresar enlace de la foto'/>
             <ListaOpciones/>
